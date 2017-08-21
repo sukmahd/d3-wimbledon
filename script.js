6e5742cacff41d5d527033fb2b8321137b33bc7e
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-08-21 15:52:32 +0700

    release 3

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const width = 750,
   margin = 20
 marginLeft = 40
 
+
 // Drawing area
 let svg = d3.select('#results')
   .append('svg')
-  .attr('width', width)
-  .attr('height', height)
+  .attr('width', width + marginLeft)
+  .attr('height', height + margin )
 
 // Data reloading
 let reload = () => {
@@ -23,30 +24,62 @@ let reload = () => {
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  const dataScore = data.map(function(dat){
+    return dat.GoalsScored
+  })
+
+  const colorScale = d3.scaleLinear()
+  .domain([0, d3.max(dataScore)])
+  .range(['red', 'yellow'])
+
 
   const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return d.GoalsScored })])
-    .range([0, height])
+    .domain([0, d3.max(dataScore)])
+    .range([0, height + margin])
 
   const xScale = d3.scaleLinear()
-    .domain([0, data.length])
+    .domain([0, dataScore.length])
+    .range([0, width])
+
+  const yAxis = d3.scaleLinear()
+    .domain([0, d3.max(dataScore)])
+    .range([height, 0])
+
+  const xAxis = d3.scaleLinear()
+    .domain([0, dataScore.length])
     .range([0, width])
 
   svg.selectAll('rect')
-    .data(data)
+    .data(dataScore)
     .enter()
     .append('rect')
+    .attr('fill', colorScale)
     .attr('class', 'bar')
     .attr('x', (d,i) => {
-      return xScale(i)
+      return xScale(i) + marginLeft - 10
     })
     .attr('y', (d) => {
-      return height - yScale(d.GoalsScored)
+      return height - yScale(d)
     })
-    .attr('width', 15)
+    .attr('width', function(d,i){
+      return xScale(1) - 1
+    })
     .attr('height', (d) => {
-      return yScale(d.GoalsScored)
+      return yScale(d)
     })
+
+    svg.append('g')
+      .attr('class', 'axisStyle')
+      .attr('transform', `translate(${marginLeft - 20})`)
+      .call(d3.axisLeft(yAxis).ticks(d3.max(dataScore)))
+
+    svg.append('g')
+      .attr('transform', `translate(${marginLeft - 10}, ${height})`)
+      .call(d3.axisBottom(xAxis).ticks(dataScore.length))
+
+
+    //Word cloud
+
 }
 
 reload()
--- topscore.js
+++ topscore.js
@@ -6,12 +6,24 @@ let leaderScale = d3.scaleLinear()
 
 const draw = (words) => {
   // Draw your data here...
-
+  cloud().size([960, 500])
+    .canvas(function() {return new Canvas(1,1); })
+    .words(words)
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font('Impact')
+    .fontSize(function(d) {return d.score})
 }
 
 
 const load = () => {
   // Load your data here...
+  d3.tsv('stats.tsv', (rows) => {
+    const list = rows.filter(function(row){
+      if(row.G > 0) return {score: row.G, name: row.Name}
+    })
+    draw(list)
+  })
 }
 
 load()
