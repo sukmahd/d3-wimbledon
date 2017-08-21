0affa35f38199e79b2b34afecda1465ee8dc3e21
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-08-21 11:36:36 +0700

    release 2

--- script.js
+++ script.js
@@ -15,12 +15,38 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', (rows) => {
+    redraw(rows)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
 
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return d.GoalsScored })])
+    .range([0, height])
+
+  const xScale = d3.scaleLinear()
+    .domain([0, data.length])
+    .range([0, width])
+
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d,i) => {
+      return xScale(i)
+    })
+    .attr('y', (d) => {
+      return height - yScale(d.GoalsScored)
+    })
+    .attr('width', 15)
+    .attr('height', (d) => {
+      return yScale(d.GoalsScored)
+    })
 }
 
 reload()
